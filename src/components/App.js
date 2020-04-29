df611b7f52fc179faeab45af38fe3521644bba2b
Serzh108 <serzh108@gmail.com>
2020-04-29 18:06:27 +0300

    HW-02 init

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import Profile from './Profile/Profile';
+// import Statistics from './Statistics/Statistics';
+// import FfiendList from './FriendList/FriendList';
+// import TransactionHistory from './TransactionHistory/TransactionHistory';
+// import user from './Profile/user.json';
+// import statisticalData from './Statistics/statistical-data.json';
+// import friends from './FriendList/friends.json';
+// import transactions from './TransactionHistory/transactions.json';
+
+const App = () => (
+  <>
+  <h2>Home work - 02 React</h2>
+    {/* <Profile {...user} /> */}
+    {/* <Statistics title="Upload stats" stats={statisticalData} /> */}
+    {/* <FfiendList friends={friends} /> */}
+    {/* <TransactionHistory items={transactions} /> */}
+  </>
+);
+export default App;
